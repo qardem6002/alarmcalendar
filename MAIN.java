4153dc278d55b998ef62ef72cff25260601acbc5
qardem6002 <nicatmemmedov2023@outlook.com>
2025-07-12 16:28:47 +0400

    Update MAIN.java
    made both arguments for ALARM function final, added @Override to public void run() (java suggests this is better, changed 
    int hours = calendar.get(Calendar.HOUR); to int hours calendar.get(Calendar.HOUR_OF_DAY); because I learned that .HOUR is 12 hour format and might not work on some systems.
    added Thread.sleep(1000); so this only checks for the time every second instead every milli second.

--- MAIN.java
+++ MAIN.java
@@ -1,23 +1,27 @@
-
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import javax.swing.JOptionPane;
 
 public class MAIN{
 
-    public void ALARM(final int a , int b){
+    public void ALARM(final int a , final int b){
         Thread thread = new Thread(){
-
+            @Override
             public void run(){
                 int k=0;
                 while (k==0){
                     Calendar calendar = new GregorianCalendar();
-                    int hours = calendar.get(Calendar.HOUR);
+                    int hours = calendar.get(Calendar.HOUR_OF_DAY);
                     int minutes = calendar.get(Calendar.MINUTE);
 
                     if (a==hours && b==minutes){
                         JOptionPane.showMessageDialog(null, "WORKS");
                         break;
+                    }
+                    try {
+                        Thread.sleep(1000); // stops for 1 second so it doesnt check every millisecond, java also says this is inside a loop so it might slow the loop
+                    } catch (InterruptedException e){
+                        e.printStackTrace(); // java wants me to use better debugging methods i think?
                     }
                 }
             }
@@ -29,7 +33,6 @@ public class MAIN{
 
     public static void main(String[] args) {
         MAIN alarm = new MAIN();
-        alarm.ALARM(4,16);
+        alarm.ALARM(16,25);
     }
 }
-
